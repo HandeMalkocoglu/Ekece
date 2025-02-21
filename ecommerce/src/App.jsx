80f842cb87b2c1b61cf722f6ea2a68aea668b35c
HandeMalkocoglu <handemalkocoglu@hotmail.com>
2025-02-21 05:22:02 +0300

    BestSeller componenti oluşturuldu

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import PageContent from "./layout/PageContent";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<PageContent />} />
+        {/* İleride diğer sayfalar eklenecekse: */}
+        {/* <Route path="/shop" element={<ShopPage />} /> */}
+        {/* <Route path="/contact" element={<ContactPage />} /> */}
       </Routes>
     </Router>
   );
 }
+
 export default App;
--- ecommerce/src/layout/Header.jsx
+++ ecommerce/src/layout/Header.jsx
@@ -1,29 +1,35 @@
 import { Link } from "react-router-dom";
 import { FaUser, FaShoppingCart, FaSearch, FaHeart } from 'react-icons/fa';
+import data from "../data/data.json";
+
 function Header() {
+  const { menu } = data;
+
   return (
     <header className="flex justify-between items-center p-4 shadow-md bg-white">
-      <h1 className="pl-50 text-lg font-bold">Bandage</h1>
+      <h1 className="text-lg font-bold">Bandage</h1>
       <nav>
-      <ul className="flex space-x-4">
-  <li><Link to="/" className="text-gray-500 hover:text-black">Home</Link></li>
-  <li><Link to="/" className="text-gray-500 hover:text-black">Shop</Link></li>
-  <li><Link to="/" className="text-gray-500 hover:text-black">About</Link></li>
-  <li><Link to="/" className="text-gray-500 hover:text-black">Blog</Link></li>
-  <li><Link to="/pricing" className="text-gray-500 hover:text-black">Contact</Link></li>
-  <li><Link to="/contact" className="text-gray-500 hover:text-black">Pages</Link></li>
-</ul>
+        <ul className="flex space-x-4">
+          {menu.map((item, index) => (
+            <li key={index}>
+              <Link to={`/${item.toLowerCase()}`} className="text-gray-500 hover:text-black">
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
-      <div className="flex space-x-4 ml-[90px]">
-  <div className="flex items-center space-x-1">
-    <FaUser className="text-blue-500 cursor-pointer" />
-    <span className="text-blue-500 cursor-pointer">Login/Register</span>
-  </div>
-  <FaShoppingCart className="text-blue-500 cursor-pointer" />
-  <FaSearch className="text-blue-500 cursor-pointer" />
-  <FaHeart className="text-blue-500 cursor-pointer" />
-</div>
+      <div className="flex space-x-4">
+        <div className="flex items-center space-x-1">
+          <FaUser className="text-blue-500" />
+          <span className="text-blue-500">Login/Register</span>
+        </div>
+        <FaShoppingCart className="text-blue-500" />
+        <FaSearch className="text-blue-500" />
+        <FaHeart className="text-blue-500" />
+      </div>
     </header>
   );
 }
+
 export default Header;
