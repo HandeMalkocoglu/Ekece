e7283a91548c186b87ca3a7d118f8578e2391cc5
HandeMalkocoglu <handemalkocoglu@hotmail.com>
2025-02-27 01:48:37 +0300

    ShopPage oluşturuldu

--- ecommerce/src/pages/ShopPage.jsx
+++ ecommerce/src/pages/ShopPage.jsx
@@ -2,56 +2,31 @@ import Header from "../layout/Header";
 import Footer from "../layout/Footer";
 import data from "../data/data.json";
 import ResultsFilter from "../components/ResultsFilter";
-import { useState, useEffect } from "react";
+import ProductGrid from "../components/ProductGrid";
+import LogoCarousel from "../components/LogoCarousel";
+
 
 function ShopPage() {
     const { banner } = data.bestSellers;
-    const { categories, products} = data.bestSellers;
-    const [selectedIndex, setSelectedIndex] = useState(0);
-      const [filteredProducts, setFilteredProducts] = useState([]);
-      // Seçili kategoriyi al
-      const selectedCategory = categories[selectedIndex];
-      // Kategori değiştiğinde ürünleri filtrele
-      useEffect(() => {
-        setFilteredProducts([]);
-        setTimeout(() => {
-          const newProducts = products.filter(
-            (product) => product.category === selectedCategory
-          );
-          setFilteredProducts(newProducts);
-        }, 0);
-      }, [selectedCategory]);
-      // Kategori değiştir
-      const handleNextCategory = () => {
-        setSelectedIndex((prevIndex) => (prevIndex + 1) % categories.length);
-      };
-      const handlePrevCategory = () => {
-        setSelectedIndex(
-          (prevIndex) => (prevIndex - 1 + categories.length) % categories.length
-        );
-      };
     return (
         <div>
+
           <Header />
           <section>
             {/* BİRİNCİ COMPONENT */}
-            <div className="flex flex-row pt-8">
-            <h1 className="text-lg font-bold pl-64">Shop</h1>
-            <div className="flex flex-row ">
-            <h1 className="text-lg font-bold pl-64">Home</h1>
-            <h1 className="text-lg font-bold pl-64 text-gray-500"> > Shop</h1>
-            </div>
+            <div>
+            <h1 className="pl-156 pr-156 text-lg font-bold">Shop</h1>
             </div>
             {/* İKİNCİ COMPONENT */}
               <div className="flex justify-center items-center gap-4 pt-8 pb-8">
-                {Array()
+                {Array(5)
                 .fill()
                 .map((_, index) => (
               <div key={index} className="relative">
         <img
           src={banner.image}
           alt="Category Banner"
-          className="w-50 h-50 object-cover shadow-md"
+          className="w-50 h-50 object-cover  shadow-md"
         />
         <div className="absolute top-2 left-2 text-black font-bold">
           <h3 className="text-xs uppercase">{banner.category}</h3>
@@ -61,40 +36,15 @@ function ShopPage() {
     ))}
 </div>
           {/*ÜÇÜNCÜ COMPONENT*/}
-          <div className="pl-64 pr-64">
+          <div className="pl-64 pr-64 pb-8">
           <ResultsFilter/>
+          <ProductGrid/>
           </div>
-          {/*DÖRDÜNCÜ COMPONENT*/}
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 mt-6 items-stretch">
-          {filteredProducts.map((product) => (
-            <div
-              key={product.id}
-              className="text-center flex flex-col items-center justify-between bg-white shadow-md rounded-lg p-4 h-full"
-            >
-              <div className="w-full h-40 flex items-center justify-center">
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="max-w-full max-h-full object-contain"
-                />
-              </div>
-              <h3 className="mt-2 text-sm font-semibold text-gray-900 pt-14">
-                {product.title}
-              </h3>
-              <p className="text-xs text-gray-500">{product.department}</p>
-              <p className="mt-1 text-sm">
-                <span className="text-gray-400 line-through">${product.price}</span>{" "}
-                <span className="text-green-500 font-semibold">
-                  ${product.discountPrice}
-                </span>
-              </p>
-            </div>
-          ))}
-        </div>
+          <LogoCarousel/>
           </section>
+          
           <Footer />
         </div>
       );
     };
-    
     export default ShopPage;
